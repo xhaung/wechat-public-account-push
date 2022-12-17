145484e08cc56a6b90f3e723c026bbdb50c66f0d
xhaung <32036737+xhaung@users.noreply.github.com>
2022-12-17 17:36:50 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'Ray',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ojfKd59wmC2uXfwFCCbXs8sv3sxI',
+      id: 'ojfKd58PkrR6rtNZr6j6YjufzQSA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'qRE0BJd6WTX4EFoPLtZW5pExR2TcF9APMBrrNNRXHTA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
