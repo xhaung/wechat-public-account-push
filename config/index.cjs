ef80dfd5c1de0275e1ea8aefc2814eb16efb1566
xhaung <32036737+xhaung@users.noreply.github.com>
2022-12-17 17:38:23 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '4b531f2b3c1e58d5434bc06d23d8378b',
 
-  PROVINCE: '英国',
-  CITY: '伦敦',
+  PROVINCE: '四川',
+  CITY: '绵阳',
 
   USERS: [
     {
