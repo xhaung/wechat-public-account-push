46e4b1e577569494b9f0715226f7a9d45ff34d96
xhaung <32036737+xhaung@users.noreply.github.com>
2023-08-15 14:09:31 +0100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,7 +47,40 @@ const USER_CONFIG = {
         // Chinese new year
         { keyword: 'cnew_year_day', date: '2023-01-22' }
       ],
-    }
+    },
+    {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ojfKd59wmC2uXfwFCCbXs8sv3sxI',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'faN1AoQfORzK6IzYeKK-wGoJgZzjPccmNGeYcKIb3Ok',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '08-25',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '1996', date: '08-25',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {
+          // type: '节日', name: '相遇', year: '2022', date: '03-19',
+        // },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-05-02' },
+        // 相遇
+        { keyword: 'meet_day', date: '2022-03-19' },
+        // easter
+        { keyword: 'easter_day', date: '2023-04-09' },
+        // Chinese new year
+        { keyword: 'cnew_year_day', date: '2023-01-22' },
+        // Going back home
+        { keyword: 'home_day', date: '2023-04-28' },
+      ],
+    },
   ],
 
 
